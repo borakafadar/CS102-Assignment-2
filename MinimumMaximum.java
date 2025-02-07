74cb8011c5054d5661ff6f5cca82c71d1db36e7f
Eylül Ündem <eylul.undem@ug.bilkent.edu.tr>
2025-02-07 18:07:24 +0300

    Added min and max method
    Added two methods for finding the minimum and maximum element of an array

---
+++ MinimumMaximum.java
@@ -0,0 +1,25 @@
+public class MinimumMaximum {
+    //the method below finds the minimum element of an array
+    public static int findMinimum (int[] array){ 
+        int minimum = array[0]; //assuming that the first element of the array is the smallest for comparison
+        for(int i = 1; i < array.length; i++){
+            if (array[i] < minimum){
+                minimum = array[i];
+            }
+        }
+        return minimum;
+    }
+
+    //the method below finds the maximum element of an array
+    public static int findMaximum (int[] array){ 
+        int maximum = array[0]; //assuming that the first element of the array is the largest for comparison
+        for(int i = 1; i < array.length; i++){
+            if (array[i] > maximum){
+                maximum = array[i];
+            }
+        }
+        return maximum;
+    }
+
+  
+}
