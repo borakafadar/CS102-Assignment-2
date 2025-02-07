ad1c1c4207311ff5acf2c29444dc4851280a76e5
Zeynel Yıldırım <zeynel.yildirim@ug.bilkent.edu.tr>
2025-02-07 14:23:24 +0300

    implemented average and differences(part d)

---
+++ Average.java
@@ -0,0 +1,27 @@
+public class d {
+    
+    public static void average(int[] nums){
+
+        double sum = 0;
+        double [] differences = new double[nums.length];
+
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+        }
+        
+        double average = sum/nums.length;
+
+        for(int i = 0; i < nums.length; i++){
+            differences[i] = average - nums[i];
+        }
+
+        System.out.println("Average: " + average);
+        System.out.print("Differences: [");
+
+        for(double element : differences){
+            System.out.print(element + ", ");
+        }
+
+        System.out.print("]");
+    } 
+}
